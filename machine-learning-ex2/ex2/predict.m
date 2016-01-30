bd5ea7a3cdc9e0ab4ff72f670ab26921cceef219
Van Huy <huyvpq@gmail.com>
2016-01-31 03:25:46 +0900

    Implement predict

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -15,7 +15,12 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
+for i = 1:m
+    h = sigmoid(X(i,:) * theta);
+    if h >= 0.5
+        p(i) = 1;
+    end
+end
 
 
 
