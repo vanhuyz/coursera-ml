7eb0728db8e00e5da419c9cb601f5d4c08bfb569
Van Huy <huyvpq@gmail.com>
2016-02-06 22:59:36 +0900

    Calculate theta

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -49,8 +49,15 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+initial_theta = zeros(n + 1, 1);
+
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 
+for c = 1:num_labels
+  all_theta(c,:) = ...
+    fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+end
 
 
 
