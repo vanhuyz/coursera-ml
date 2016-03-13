92bcd67818c8d5517d2b0ecb950d7b5b4c8e43e8
Van Huy <huyvpq@gmail.com>
2016-03-13 18:16:31 +0900

    Plot errors

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,23 +23,36 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
-minError = 1;
+C_vec = sigma_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+errors = zeros(length(C_vec));
 
-for tC = values
-  for tSigma = values
-    model= svmTrain(X, y, tC, @(x1, x2) gaussianKernel(x1, x2, tSigma));
+for i = 1:length(C_vec)
+  for j = 1:length(sigma_vec)
+    model= svmTrain(X, y, C_vec(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(j)));
     predictions = svmPredict(model, Xval);
-    predictionError = mean(double(predictions ~= yval));
-    if predictionError < minError
-      C = tC;
-      sigma = tSigma;
-      minError = predictionError;
-    end
+    errors(i,j) = mean(double(predictions ~= yval));
   end
 end
 
+[_,I] = min(errors(:));
+[I_row,I_col] = ind2sub(size(errors),I);
+
+C = C_vec(I_row)          % 1
+sigma = sigma_vec(I_col)  % 0.1
 
 % =========================================================================
+% plot
+p = zeros(length(sigma_vec),1);
+colors = ['k','r','g','b','m','c','k','r'];
+styles = ['-','-','-','-','-','-','--','--'];
+for j = 1:length(sigma_vec)
+  p(j) = semilogx(C_vec, errors(j,:),'color',colors(j),'linestyle',styles(j));
+  legend(p(j),sprintf("%0.2f",sigma_vec(j)));
+  hold on;
+end
+xlabel('C');
+ylabel('Error');
+title('sigma')
+hold off;
 
 end
