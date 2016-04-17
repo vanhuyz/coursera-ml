4907a295cfc23afa7bb31a68abb5dbe82f5b3b13
Van Huy <huyvpq@gmail.com>
2016-04-18 01:19:45 +0900

    Add my rating list

--- machine-learning-ex8/ex8/ex8_cofi.m
+++ machine-learning-ex8/ex8/ex8_cofi.m
@@ -128,22 +128,24 @@ my_ratings = zeros(1682, 1);
 
 % Check the file movie_idx.txt for id of each movie in our dataset
 % For example, Toy Story (1995) has ID 1, so to rate it "4", you can set
-my_ratings(1) = 4;
+% my_ratings(1) = 4;
 
-% Or suppose did not enjoy Silence of the Lambs (1991), you can set
-my_ratings(98) = 2;
+% % Or suppose did not enjoy Silence of the Lambs (1991), you can set
+% my_ratings(98) = 2;
 
-% We have selected a few movies we liked / did not like and the ratings we
-% gave are as follows:
-my_ratings(7) = 3;
-my_ratings(12)= 5;
-my_ratings(54) = 4;
-my_ratings(64)= 5;
-my_ratings(66)= 3;
-my_ratings(69) = 5;
-my_ratings(183) = 4;
-my_ratings(226) = 5;
-my_ratings(355)= 5;
+% % We have selected a few movies we liked / did not like and the ratings we
+% % gave are as follows:
+% my_ratings(7) = 3;
+% my_ratings(12)= 5;
+% my_ratings(54) = 4;
+% my_ratings(64)= 5;
+% my_ratings(66)= 3;
+% my_ratings(69) = 5;
+% my_ratings(183) = 4;
+% my_ratings(226) = 5;
+% my_ratings(355)= 5;
+
+my_ratings = myRatings();
 
 fprintf('\n\nNew user ratings:\n');
 for i = 1:length(my_ratings)
---
+++ machine-learning-ex8/ex8/myRatings.m
@@ -0,0 +1,59 @@
+function my_ratings = myRatings()
+
+my_ratings = zeros(1682, 1);
+
+% 1 Toy Story (1995)
+my_ratings(1) = 4;
+
+% 50 Star Wars (1977)
+my_ratings(50) = 5;
+
+% 56 Pulp Fiction (1994)
+my_ratings(56) = 1;
+
+% 64 Shawshank Redemption, The (1994)
+my_ratings(64) = 5;
+
+% 69 Forrest Gump (1994)
+my_ratings(69) = 5;
+
+% 71 Lion King, The (1994)
+my_ratings(71) = 4;
+
+% 72 Mask, The (1994)
+my_ratings(72) = 3;
+
+% 95 Aladdin (1992)
+my_ratings(95) = 5;
+
+% 99 Snow White and the Seven Dwarfs (1937)
+my_ratings(99) = 3;
+
+% 127 Godfather, The (1972)
+my_ratings(127) = 5;
+
+% 172 Empire Strikes Back, The (1980)
+my_ratings(172) = 5;
+
+% 181 Return of the Jedi (1983)
+my_ratings(181) = 5;
+
+% 187 Godfather: Part II, The (1974)
+my_ratings(187) = 5;
+
+% 204 Back to the Future (1985)
+my_ratings(204) = 5;
+
+% 257 Men in Black (1997)
+my_ratings(257) = 4;
+
+% 313 Titanic (1997)
+my_ratings(313) = 3;
+
+% 483 Casablanca (1942)
+my_ratings(483) = 3;
+
+% 894 Home Alone 3 (1997)
+my_ratings(894) = 5;
+
+end
